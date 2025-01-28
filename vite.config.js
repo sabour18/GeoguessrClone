47c5b03d2dba27007edc97c457a97b871becbe7f
Bailey <sabour047@gmail.com>
2025-01-27 19:27:18 -0600

    chang base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicPath: '/GeogussrClone/',
+  publicPath: '/GeoguessrClone/',
   plugins: [
     vue(),
     vueDevTools(),
